5daac8e03c74dfb3c57b1826fc3c3de5ce18ebb9
Guillermo Rojas <guillermorojaslema@gmail.com>
2013-07-07 20:31:02 -0400

    dos tablas creadas con schema

---
+++ portal/laravel/application/migrations/2013_07_08_002541_estudio.php
@@ -0,0 +1,34 @@
+<?php
+
+class Estudio {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// código para crear la tabla estudio
+		Schema::create('estudio', function ($tabla){
+			$tabla->increments('id');
+			$tabla->text('nombre');
+			$tabla->string('descripcion', 255); 
+			$tabla->date('fecha_inicio');
+			$tabla->boolean('situacion'); // falso como incompleto
+			$tabla->string('ubicacion');
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// código para eliminar la tabla
+		Schema::drop('estudio');
+	}
+
+}
