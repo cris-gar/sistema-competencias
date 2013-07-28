f9acff48e4e759e4a0917b89a8d6fd03146dfa3e
ljofre <ljofre2146@gmail.com>
2013-07-28 13:58:57 -0400

    creación del widget como objeto javascript, interactividad de los widget en el index por medio de jqueryui

--- portal/Posiblecss/bluemasters/js/widgets.js
+++ portal/Posiblecss/bluemasters/js/widgets.js
@@ -1,20 +1,60 @@
-
 //widget con la informacion minima visible de un profesor
-var Profesor_widget = function(json_data){
-	/*json_data diccionario con la informacion basica del profesor
-	return: widget con la informacion basica*/
-	
-	var template = "<div class=\"resume\">"+
-			"<img src=img_url><div><h3>name</h3><h4>depto</h4></div>"+
-	"</div>";
-	
-	var name = json_data.name;
-	var depto = json_data.depto;
-	var img_url = json_data.img_url;
+var Profesor_widget = function(json_data) {
+	/*
+	 * json_data diccionario con la informacion basica del profesor return:
+	 * widget con la informacion basica
+	 */
 
-	template = template.replace("name", name);
-	template = template.replace("depto", depto);
-	template = template.replace("img_url", img_url);
+	this.template = "<div class=@resume>"
+			+ "<img class = @img_perfil src=img_url><div class=@data><h3>name</h3><h4>depto</h4></div>"
+			+ "</div>";
+
+	this.name = json_data.name;
+	this.depto = json_data.depto;
+	this.img_url = json_data.img_url;
+
+	/* agregar los datos a la plantilla */
+	this.template = this.template.replace("name", this.name);
+	this.template = this.template.replace("depto", this.depto);
+	this.template = this.template.replace("img_url", this.img_url);
+
+	/* setear los estilos en el widget */
+	this.template = this.template.replace("@resume", "resume")
+	this.template = this.template.replace("@img_perfil", "img_perfil")
+	this.template = this.template.replace("@data", "data")
 	
-	document.write(template)	
+	/*metodos de la clase*/
+		
+	/*escribir el contenido del widget*/
+	function write() {
+		document.write(this.template);
+	}
+	
 }
+
+$(function() {
+	$("#sortable").sortable();
+	$("#sortable").disableSelection();
+});
+
+$(".resume").click(function() {
+	$(this).css({
+		"width" : "40%"
+	});
+})
+
+var ajax_rand_profesores = function() {
+	/* implementacion aqui */
+}
+
+/* evento cuando se carga pa pagina y carga a todos los profesores */
+$(window).onload(function() {
+	/* por medio de ajax extraemos las cartas de presentacion */
+
+	var profesores = ajax_rand_profesores();
+
+	for (profesor in profesores) {
+
+		/* esperar un momento antes de cargar al nuevo profesor */
+	}
+})
