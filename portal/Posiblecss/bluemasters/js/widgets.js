911feee96689b8b3a04182bb157933e8ac1e4f84
ljofre <ljofre2146@gmail.com>
2013-07-30 22:02:37 -0400

    algo de interactividad en el diseño

--- portal/Posiblecss/bluemasters/js/widgets.js
+++ portal/Posiblecss/bluemasters/js/widgets.js
@@ -1,12 +1,16 @@
+var curriculum_widget = function(data) {
+
+	this.name = data.name;
+	this.picture = data.picture;
+
+}
 //widget con la informacion minima visible de un profesor
 var Profesor_widget = function(data) {
 	/*
 	 * json_data diccionario con la informacion basica del profesor return:
 	 * widget con la informacion basica
 	 */
-	this.template = "<div class=@resume>"
-			+ "<img class = @img_perfil src=img_url><div class=@data><h3>name</h3><h4>depto</h4></div>"
-			+ "</div>";
+	this.template = "<div class='@resume'><img class = '@img_perfil' src=img_url><div class='@data'><h3>name</h3><h4>depto</h4></div></div>";
 
 	this.name = data.name;
 	this.depto = data.depto;
@@ -18,42 +22,31 @@ var Profesor_widget = function(data) {
 	this.template = this.template.replace("img_url", this.img_url);
 
 	/* setear los estilos en el widget */
-	this.template = this.template.replace("@resume", "resume")
-	this.template = this.template.replace("@img_perfil", "img_perfil")
-	this.template = this.template.replace("@data", "data")
+	this.template = this.template.replace("@resume", "resume");
+	this.template = this.template.replace("@img_perfil", "img_perfil");
+	this.template = this.template.replace("@data", "data");
 
-	/* metodos de la clase */
-
-	/* escribir el contenido del widget */
-	function write() {
-		document.write(this.template);
-	}
-
-}
+};
 
 $(function() {
 	$("#sortable").sortable();
 	$("#sortable").disableSelection();
 });
 
-$(".resume").click(function() {
-	$(this).css({
-		"width" : "40%"
-	});
-})
+/*al cargar la pagina*/
+$(document).ready(function() {
+	/*cargar cartas de presentacion profesores*/
+	//profesores json = ajax_rand_profesores();
+	/*mostrar cada uno de los profesores en distintos tiempos de latencia*/
+	$(".resume").mouseover(function() {
+		$(".resume").animate({
+			width : "40%"
+		});
+	})
 
-var ajax_rand_profesores = function() {
-	/* implementacion aqui */
-}
-
-/* evento cuando se carga pa pagina y carga a todos los profesores */
-$(window).onload(function() {
-	/* por medio de ajax extraemos las cartas de presentacion */
-
-	var profesores = ajax_rand_profesores();
-
-	for (profesor in profesores) {
-
-		/* esperar un momento antes de cargar al nuevo profesor */
-	}
-})
+	$(".resume").mouseout(function() {
+		$(".resume").animate({
+			width : "30%"
+		});
+	})
+});
